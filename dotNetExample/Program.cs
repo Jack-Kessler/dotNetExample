231700cbe9d56d966a7d65bdbca1335967521847
Jack Kessler <jack_kessler@hotmail.com>
2023-01-27 11:47:48 -0600

    Third commit

--- dotNetExample/Program.cs
+++ dotNetExample/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("I am chainging this line of text.");
+Console.WriteLine("I am adding this line of code just as a check");
